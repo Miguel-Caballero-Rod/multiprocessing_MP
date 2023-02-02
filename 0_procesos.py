55f5cae655e6bf849b7f4c55152fca361f7a3776
Miguel-Caballero-Rod <micaba03@ucm.es>
2023-02-02 10:38:06 +0100

    conflicto

--- 0_procesos.py
+++ 0_procesos.py
@@ -6,7 +6,6 @@ https://docs.python.org/3.6/library/multiprocessing.html
 """
 from multiprocessing import Process
 def f():
-<<<<<<< HEAD
    for i in range(5):
       print ("hola", i)
       sleep(random())
@@ -16,14 +15,3 @@ if __name__ == "__main__":
    p.start()
    q.start()
    print ("fin")
-=======
-for i in range(5):
-print ("hola", i)
-sleep(random())
-if __name__ == "__main__":
-p = Process(target=f)
-q = Process(target=f)
-p.start()
-q.start()
-print ("fin")
->>>>>>> 557304cbca84537ea9c1454c95de2ed85ca315a5
