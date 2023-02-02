e190d34742937d772281fcc644c9f5ce14bf12b3
Pedro <pecorral@ucm.es>
2023-02-02 10:51:25 +0100

    corrected identation in file 2

--- 2_procesos.py
+++ 2_procesos.py
@@ -4,9 +4,9 @@ from multiprocessing import Process
 from multiprocessing import current_process
 def f():
    for i in range(5):
-   print (f"hola soy {current_process().name}, " + \
+      print (f"hola soy {current_process().name}, " + \
       f"{current_process().pid}, {current_process().is_alive()}, vuelta {i}")
-   time.sleep(random.random()/3)
+      time.sleep(random.random()/3)
 def g():
    print ("adios")
 if __name__ == "__main__":
@@ -14,7 +14,7 @@ if __name__ == "__main__":
    lp = []
    for i in range(N):
       lp.append(Process(target=f, name=f"ana_{i}"))
-      for p in lp:
+   for p in lp:
       p.start()
    q = Process(target=g)
    q.start()
